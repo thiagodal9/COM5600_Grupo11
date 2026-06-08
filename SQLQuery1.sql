fe39309742f9363c88d47b7ceaf2fb310bd51323
thiagodal9 <127763499+thiagodal9@users.noreply.github.com>
2026-06-08 00:48:00 -0300

    Creación de Base de datos, Esquema y tablas

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -16,17 +16,17 @@ go
 use ParquesNacionales
 go
 
---Creo el esquema donde voy a guardar los parques
---create schema Parques
---go
+--Creo el esquema donde voy a crear las tablas
+create schema PnTablas
+go
 
 --Creación de tablas
 
 --Tabla Persona
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Persona')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Persona')
 begin
-create table Parques.Persona(
+create table PnTablas.Persona(
 idPersona int identity(1,1) primary key,
 dni int check(dni > 0),
 nombre char(20),
@@ -41,9 +41,9 @@ go
 
 --Tabla Provincia
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Provincia')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Provincia')
 begin
-create table Parques.Provincia(
+create table PnTablas.Provincia(
 idProvincia int identity(1,1) primary key,
 nombre char(30)
 )
@@ -52,9 +52,9 @@ go
 
 --Tabla TipoParque
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'TipoParque')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'TipoParque')
 begin
-create table Parques.TipoParque(
+create table PnTablas.TipoParque(
 idTipoParque int identity(1,1) primary key,
 descripcion varchar
 )
@@ -64,16 +64,16 @@ go
 
 --Tabla Parque
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Parque')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Parque')
 begin
-create table Parques.Parque(
+create table PnTablas.Parque(
 idParque int identity(1,1) primary key,
 idTipoParque int,
 idProv int,
 nombre char (50),
 superficieM2 decimal(10,2),
-constraint FK_Parque_TipoParque(idTipoParque) references Parques.TipoParque(idTipoParque),
-constraint FK_Parque_Provincia(idProvincia) references Parques.Provincia(idProvincia)
+constraint FK_Parque_TipoParque(idTipoParque) references PnTablas.TipoParque(idTipoParque),
+constraint FK_Parque_Provincia(idProvincia) references PnTablas.Provincia(idProvincia)
 )
 end
 go
@@ -81,9 +81,9 @@ go
 
 --Tabla HorarioParque
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'HorarioParque')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'HorarioParque')
 begin
-create table Parques.HorarioParque(
+create table PnTablas.HorarioParque(
 idHorario int identity(1,1) primary key,
 idParque int,
 horaApertura time,
@@ -91,64 +91,64 @@ horaCierre time,
 temporada char(10),
 ubicacion char(30),
 dia date,
-constraint FK_HorarioParque_Parque(idParque) references Parques.Parque(idParque),
+constraint FK_HorarioParque_Parque(idParque) references PnTablas.Parque(idParque),
 )
 end
 go
 
 --Tabla GuardaParque
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'GuardaParque')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'GuardaParque')
 begin
-create table Parques.GuardaParque(
+create table PnTablas.GuardaParque(
 idGuardaParque int identity(1,1)
 idPersona int,
 idParque int,
 estado bool,
 fechaInicio date,
 constraint PK_GuardaParque(idGuardaParque, idPersona, idParque),
-constraint FK_GuardaParque_Persona (idPersona) references Parques.Persona(idPersona),
-constraint FK_GuardaParque_Parque (idParque) references Parques.Parque(idParque),
+constraint FK_GuardaParque_Persona (idPersona) references PnTablas.Persona(idPersona),
+constraint FK_GuardaParque_Parque (idParque) references PnTablas.Parque(idParque),
 )
 end
 go
 
 --Tabla Historial
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Historial')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Historial')
 begin
-create table Parques.Historial(
+create table PnTablas.Historial(
 fechaIngreso date,
 idGuardaParque int,
 razonEgreso varchar,
 fechaEgreso date,
 constraint PK_Historial(fechaIngreso, idGuardaParque),
-constraint FK_Historial_GuardaParque (idGuardaParque) references Parques.GuardaParque(idGuardaParque)
+constraint FK_Historial_GuardaParque (idGuardaParque) references PnTablas.GuardaParque(idGuardaParque)
 )
 end
 go
 
 --Tabla Guia
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Guia')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Guia')
 begin
-create table Parques.Guia(
+create table PnTablas.Guia(
 idGuia int identity(1,1),
 idPersona int,
 titulo varchar,
 vencimiento Habilitacion date,
 numeroHabilitacion int,
 constraint PK_Guia(idGuia, idPersona),
-constraint FK_Guia_Persona (idPersona) references Parques.Persona(idPersona)
+constraint FK_Guia_Persona (idPersona) references PnTablas.Persona(idPersona)
 )
 end
 go
 
 --Tabla Especialidad
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Especialidad')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Especialidad')
 begin
-create table Parques.Especialidad(
+create table PnTablas.Especialidad(
 idEspecialidad int identity(1,1) primary key,
 descripcion varchar,
 )
@@ -157,16 +157,16 @@ go
 
 --Tabla GuiaEspecialidad
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'GuiaEspecialidad')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'GuiaEspecialidad')
 begin
-create table Parques.GuiaEspecialidad(
+create table PnTablas.GuiaEspecialidad(
 idGuia int,
 idPersona int,
 idEspecialidad int,
 constraint PK_GuiaEspecialidad(idGuia, idPersona, idEspecialidad),
-constraint FK_GuiaEspecialidad_Guia (idGuia) references Parques.Guia(idGuia),
-constraint FK_GuiaEspecialidad_Persona (idPersona) references Parques.Persona(idPersona),
-constraint FK_GuiaEspecialidad_Especialidad(idEspecialidad) references Parques.Especialidad(idEspecialidad)
+constraint FK_GuiaEspecialidad_Guia (idGuia) references PnTablas.Guia(idGuia),
+constraint FK_GuiaEspecialidad_Persona (idPersona) references PnTablas.Persona(idPersona),
+constraint FK_GuiaEspecialidad_Especialidad(idEspecialidad) references PnTablas.Especialidad(idEspecialidad)
 )
 end
 go
@@ -174,9 +174,9 @@ go
 
 --Tabla TipoActividad
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'TipoActividad')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'TipoActividad')
 begin
-create table Parques.TipoActividad(
+create table PnTablas.TipoActividad(
 idTipoActividad int identity(1,1) primary key,
 descripcion varchar,
 costo decimal(10,2)
@@ -186,9 +186,9 @@ go
 
 --Tabla Actividad
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Actividad')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Actividad')
 begin
-create table Parques.Actividad(
+create table PnTablas.Actividad(
 idActividad int identity(1,1) primary key,
 idParque int,
 idGuia int,
@@ -196,9 +196,9 @@ idTipoActividad int,
 nombre char(50),
 duracionMinutos int,
 cupoMax int,
-constraint FK_Actividad_Parque (idParque) references Parques.Parque(idParque),
-constraint FK_Actividad_Guia (idGuia) references Parques.Guia(idGuia),
-constraint FK_Actividad_TipoActividad (idTipoActividad) references Parques.TipoActividad(idTipoActividad)
+constraint FK_Actividad_Parque (idParque) references PnTablas.Parque(idParque),
+constraint FK_Actividad_Guia (idGuia) references PnTablas.Guia(idGuia),
+constraint FK_Actividad_TipoActividad (idTipoActividad) references PnTablas.TipoActividad(idTipoActividad)
 )
 end
 go
@@ -206,22 +206,22 @@ go
 
 --Tabla HorarioActividad
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'HorarioActividad')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'HorarioActividad')
 begin
-create table Parques.HorarioActividad(
+create table PnTablas.HorarioActividad(
 fechaActividad date primary key,
 idActividad int,
 horaInicion time,
-constraint FK_HorarioActividad_Actividad (idActividad) references Parques.Actividad(idActividad),
+constraint FK_HorarioActividad_Actividad (idActividad) references PnTablas.Actividad(idActividad),
 )
 end
 go
 
 --Tabla Venta
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Venta')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Venta')
 begin
-create table Parques.Venta(
+create table PnTablas.Venta(
 idVenta int identity(1,1) primary key,
 idActividad int,
 idTipoActividad int,
@@ -229,26 +229,26 @@ fechaActividad date,
 idParque int,
 fechaVenta date,
 totalVenta decimal(10,2),
-constraint FK_Venta_Actividad (idActividad) references Parques.Actividad(idActividad),
-constraint FK_Venta_TipoActividad (idTipoActividad) references Parques.TipoActividad(idTipoActividad),
-constraint FK_Venta_FechaActividad (fechaActividad) references Parques.HorarioActividad(fechaActividad),
-constraint FK_Venta_Parque (idParque) references Parques.Parque(idParque)
+constraint FK_Venta_Actividad (idActividad) references PnTablas.Actividad(idActividad),
+constraint FK_Venta_TipoActividad (idTipoActividad) references PnTablas.TipoActividad(idTipoActividad),
+constraint FK_Venta_FechaActividad (fechaActividad) references PnTablas.HorarioActividad(fechaActividad),
+constraint FK_Venta_Parque (idParque) references PnTablas.Parque(idParque)
 )
 end
 go
 
 --Tabla PagoVenta
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'PagoVenta')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'PagoVenta')
 begin
-create table Parques.PagoVenta(
+create table PnTablas.PagoVenta(
 idPagoVenta int identity(1,1) primary key,
 idVenta int,
 importe decimal(10,2),
 fechaPago date,
 descripcion varchar,
 metodo char(30),
-constraint FK_PagoVenta_Venta (idVenta) references Parques.Venta(idVenta)
+constraint FK_PagoVenta_Venta (idVenta) references PnTablas.Venta(idVenta)
 )
 end
 go
@@ -256,24 +256,24 @@ go
 
 --Tabla Entrada
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Entrada')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Entrada')
 begin
-create table Parques.Entrada(
+create table PnTablas.Entrada(
 idEntrada int identity(1,1) primary key,
 idVenta int,
 precio decimal(10,2),
 descripcion varchar,
 cantidad int,
-constraint FK_Entrada_Venta (idVenta) references Parques.Venta(idVenta)
+constraint FK_Entrada_Venta (idVenta) references PnTablas.Venta(idVenta)
 )
 end
 
 
 --Tabla Empresa
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Empresa')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Empresa')
 begin
-create table Parques.Empresa(
+create table PnTablas.Empresa(
 idEmpresa int identity(1,1) primary key,
 nombre char(50),
 descripcion varchar,
@@ -283,9 +283,9 @@ end
 
 --Tabla Concesion
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'Concesion')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'Concesion')
 begin
-create table Parques.Concesion(
+create table PnTablas.Concesion(
 idConcesion int identity(1,1) primary key,
 idParque int,
 idEmpresa int,
@@ -293,22 +293,22 @@ rubro char(30),
 fechaInicioConcesion date,
 fechaFinConcesion date,
 costoAlquiler decimal(10,2),
-constraint FK_Concesion_Parque (idParque) references Parques.Parque(idParque),
-constraint FK_Concesion_Empresa (idEmpresa) references Parques.Empresa(idEmpresa)
+constraint FK_Concesion_Parque (idParque) references PnTablas.Parque(idParque),
+constraint FK_Concesion_Empresa (idEmpresa) references PnTablas.Empresa(idEmpresa)
 )
 end
 
 --Tabla HistorialPago
 if not exists (SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE
-TABLE_SCHEMA = 'Parques' AND TABLE_NAME = 'HistorialPago')
+TABLE_SCHEMA = 'PnTablas' AND TABLE_NAME = 'HistorialPago')
 begin
-create table Parques.HistorialPago(
+create table PnTablas.HistorialPago(
 idPagoConcesion int identity(1,1) primary key,
 idConcesion int,
 fechaPagoConcesion date,
 importe decimal(10,2),
 estado bool,
-constraint FK_PagoConcesion_Concesion (idConsecion) references Parques.Consecion(idConsecion)
+constraint FK_PagoConcesion_Concesion (idConsecion) references PnTablas.Consecion(idConsecion)
 )
 end
 
